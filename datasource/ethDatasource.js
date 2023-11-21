34587b9524d67533b8533e16221819457c563a59
Mukhuuu <98751228+Mukhuuu@users.noreply.github.com>
2023-11-21 23:51:32 +0100

    Update ethDatasource.js

--- datasource/ethDatasource.js
+++ datasource/ethDatasource.js
@@ -1,42 +1,44 @@
-const { RESTDataSource } = require("apollo-datasource-rest");
+const { RESTDataSource } = require("apollo-datasource-rest"); // Import the RESTDataSource class from the apollo-datasource-rest package
 
 //Vitalik's Ethereum Address
 const eth_address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045";
 
-//Etherscan Data Source Class
+// Define the EtherDataSource class as a subclass of RESTDataSource
 class EtherDataSource extends RESTDataSource {
-    constructor() {
-        super();
-        this.baseURL = "https://api.etherscan.io/api";
-    }
+  constructor() {
+    super();
+    // Set the base URL for API requests to Etherscan
+    this.baseURL = "https://api.etherscan.io/api";
+  }
 
-    async etherBalanceByAddress() {
-        return this.get(
-            `?module=account&action=balance&address=${eth_address}&tag=latest&apikey=${process.env.ETHERSCAN_API}`
-        );
-    }
+  // Fetch the Ethereum balance for the specified address
+  async etherBalanceByAddress() {
+    return this.get(
+      `?module=account&action=balance&address=${eth_address}&tag=latest&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
 
-    async totalSupplyOfEther() {
-        return this.get(
-            `?module=stats&action=ethsupply&apikey=${process.env.ETHERSCAN_API}`
-        );
-    }
+  // Fetch the total supply of Ethereum
+  async totalSupplyOfEther() {
+    return this.get(
+      `?module=stats&action=ethsupply&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
 
-    //Paste Code Here For New API Endpoints
+  // Fetch the latest Ethereum price
+  async getLatestEthereumPrice() {
+    return this.get(
+      `?module=stats&action=ethprice&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
 
-    // Get latest Ethereum price
-    async getLatestEthereumPrice() {
-        return this.get(
-            `?module=stats&action=ethprice&apikey=${process.env.ETHERSCAN_API}`
-        );
-    }
-
-    // Get block confirmation time
-    async getBlockConfirmationTime() {
-        return this.get(
-            `?module=gastracker&action=gasestimate&gasprice=2000000000&apikey=${process.env.ETHERSCAN_API}`
-        );
-    }
+  // Fetch the estimated block confirmation time
+  async getBlockConfirmationTime() {
+    return this.get(
+      `?module=gastracker&action=gasestimate&gasprice=2000000000&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
 }
 
+// Export the EtherDataSource class for use in other modules
 module.exports = EtherDataSource;
